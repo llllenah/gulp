f9dd866bd20f2d63a9e3a93eccb6a34d7c45ae29
Olena Serhiienko <lena066636@gmail.com>
2024-07-15 22:04:53 +0300

    gulp.sass-glob, replaced header and footer with common name

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const fileInclude = require('gulp-file-include');
 const sass = require('gulp-sass')(require('sass'));
-var sassGlob = require('gulp-sass-glob')
+const sassGlob = require('gulp-sass-glob')
 const server = require('gulp-server-livereload');
 const clean = require('gulp-clean');
 const fs = require('fs');
@@ -60,6 +60,7 @@ gulp.task('sass',function () {
         .pipe(changed('./dist/css/'))
         .pipe(plumber(plumberNotify('Styles')))
         .pipe(sourceMaps.init())
+        .pipe(sassGlob())
         .pipe(sass())
         .pipe(groupMedia())
         .pipe(sourceMaps.write())
